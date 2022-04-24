7d86d9d317e7531b4327f393859b374b40890aa6
oktntko <oktntko@gmail.com>
2022-04-24 19:32:22 +0900

    [pages] 404 Not Found

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import { DashboardLayout } from "~/layouts/dashboard";
 import { EmptyLayout } from "~/layouts/empty";
 import { IndexPage } from "~/pages";
+import { NotFoundPage } from "~/pages/404";
 import { HelpPage } from "~/pages/help";
 import { InvoicePage } from "~/pages/invoices";
 import { LandingPage } from "~/pages/landing";
@@ -18,6 +19,7 @@ export function AppRoute() {
       </Route>
       <Route element={<EmptyLayout />}>
         <Route path="/landing" element={<LandingPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Route>
     </Routes>
   );
