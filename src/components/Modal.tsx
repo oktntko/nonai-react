801f77bff782bd89b161a06293032d0fbacb2a53
oktntko <oktntko@gmail.com>
2022-04-30 16:55:29 +0900

    [table]

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -39,7 +39,7 @@ export default function MyModal({
           leaveTo="opacity-0"
         >
           {/* 背景 */}
-          <Dialog.Overlay className="fixed inset-0 bg-slate-800 bg-opacity-50" />
+          <Dialog.Overlay className="fixed inset-0 bg-slate-800/50 backdrop-blur-[2px]" />
         </Transition.Child>
 
         <Transition.Child
@@ -51,8 +51,10 @@ export default function MyModal({
           leaveFrom="opacity-100 scale-100"
           leaveTo="opacity-0 scale-[0.8]"
         >
-          {/* コンテンツ。アニメーションだけ。focusできる要素がないとwarningになる。 */}
-          {children}
+          <div className="relative h-full w-full max-w-2xl p-4 md:h-auto">
+            {/* コンテンツ。アニメーションだけ。focusできる要素がないとwarningになる。 */}
+            {children}
+          </div>
         </Transition.Child>
       </Dialog>
     </Transition>
